395fffa203484048de2f1347fbe395d4171ce969
eehanseul <gkstmf616@gmail.com>
2024-08-04 18:35:07 +0900

    feat: add UsersDto validation

--- src/main/java/shinhanIntern/shinhan/user/dto/UsersDto.java
+++ src/main/java/shinhanIntern/shinhan/user/dto/UsersDto.java
@@ -1,5 +1,8 @@
 package shinhanIntern.shinhan.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Builder
@@ -8,7 +11,11 @@ import lombok.*;
 @Setter
 @AllArgsConstructor
 public class UsersDto {
+    @NotNull
     private Long id;
+    @Email
+    @NotBlank(message = "이메일이 없습니다.")
     private String email;
+    @NotBlank(message = "이름이 없습니다.")
     private String name;
 }
