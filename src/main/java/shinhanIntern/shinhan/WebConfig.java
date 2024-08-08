80d05d941350f7cd45354128cb5cd4362b448aa3
Tomk2d <uijinshin01@gmail.com>
2024-08-08 13:47:36 +0900

    fix Webconfig

--- src/main/java/shinhanIntern/shinhan/WebConfig.java
+++ src/main/java/shinhanIntern/shinhan/WebConfig.java
@@ -4,14 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    private static final String DEVELOP_FRONT_ADDRESS = "http://localhost:5173";
 
-    @Override
+public class WebConfig{
+    private static final String DEVELOP_FRONT_ADDRESS = "http://133.186.251.189:5173";
+
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins(DEVELOP_FRONT_ADDRESS)
+            .allowedOrigins("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .exposedHeaders("location")
             .allowedHeaders("*")
