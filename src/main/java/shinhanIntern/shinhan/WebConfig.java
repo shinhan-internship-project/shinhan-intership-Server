cfa4091fffecb5a4eac0050866f6fd09e7722668
Tomk2d <150140536+Tomk2d@users.noreply.github.com>
2024-08-08 13:45:19 +0900

    Update WebConfig.java

--- src/main/java/shinhanIntern/shinhan/WebConfig.java
+++ src/main/java/shinhanIntern/shinhan/WebConfig.java
@@ -4,11 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+
 public class WebConfig implements WebMvcConfigurer {
     private static final String DEVELOP_FRONT_ADDRESS = "http://localhost:5173";
 
-    @Override
+
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins(DEVELOP_FRONT_ADDRESS)
