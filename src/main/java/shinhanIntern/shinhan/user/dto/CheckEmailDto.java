dc1f7bcb2c1886d22c08bc7918328842e9a92ee4
Tomk2d <uijinshin01@gmail.com>
2024-08-08 20:54:17 +0900

    이메일 중복체크 업데이트

---
+++ src/main/java/shinhanIntern/shinhan/user/dto/CheckEmailDto.java
@@ -0,0 +1,18 @@
+package shinhanIntern.shinhan.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CheckEmailDto {
+    private String email;
+}
