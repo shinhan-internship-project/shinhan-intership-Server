36565d7de33c77c4c18963e77bcc1efc9f926e64
eehanseul <gkstmf616@gmail.com>
2024-08-08 21:46:45 +0900

    feat: pblist paging

--- src/main/java/shinhanIntern/shinhan/mainPage/MainPageRestController.java
+++ src/main/java/shinhanIntern/shinhan/mainPage/MainPageRestController.java
@@ -4,9 +4,13 @@ package shinhanIntern.shinhan.mainPage;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import shinhanIntern.shinhan.mainPage.domain.PbListView;
+import shinhanIntern.shinhan.mainPage.domain.PbListViewNew;
 import shinhanIntern.shinhan.mainPage.dto.PbDetailDto;
 import shinhanIntern.shinhan.mainPage.dto.PbUserDto;
 import shinhanIntern.shinhan.mainPage.service.PbUserService;
@@ -20,14 +24,29 @@ import shinhanIntern.shinhan.utils.ApiUtils.ApiResult;
 public class MainPageRestController {
     private final PbUserService pbUserService;
 
+//    @GetMapping("/pbList")
+//    public ApiResult<List<PbListView>> list(
+//            @RequestParam(value = "distance", defaultValue = "false") boolean isDistance
+//    ) {
+//        try{
+//            List<PbListView> pbListView = pbUserService.getPbView(isDistance);
+//            return ApiUtils.success(pbListView);
+//        }catch(NullPointerException e){
+//            return ApiUtils.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//    }
+
     @GetMapping("/pbList")
-    public ApiResult<List<PbListView>> list(
-            @RequestParam(value = "distance", defaultValue = "false") boolean isDistance
+    public ApiResult<Page<PbListViewNew>> list(
+            @RequestParam(value = "distance", defaultValue = "false") boolean isDistance,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size
     ) {
-        try{
-            List<PbListView> pbListView = pbUserService.getPbView(isDistance);
-            return ApiUtils.success(pbListView);
-        }catch(NullPointerException e){
+        try {
+            Pageable pageable = PageRequest.of(page, size);
+            Page<PbListViewNew> pbListViewNew = pbUserService.getPbView(isDistance, pageable);
+            return ApiUtils.success(pbListViewNew);
+        } catch (NullPointerException e) {
             return ApiUtils.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
